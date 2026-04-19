67c46922b934d60600ae15987a7ee5b749076300
Erika Kavuto <erika.gwiyo@student.moringaschool.com>
2026-04-19 05:40:41 +0300

    Changed my API_key

--- services.js
+++ services.js
@@ -1,17 +1,18 @@
-export async function getAuthToken() {
-const response = await fetch("", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    api_key: "fs_gH660FFwT1BpUqvmhQCbgvoB3HiKkzv2",
-    device_name: "My App",
-  }),
+const WEATHER_API_KEY = '398133a7a6msh51384cbccc390e9p1762e6jsnbfab717f1144';
+const data = null;
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
 });
 
-const data = await response.json();
-return data.token;
-}
+xhr.open('GET', 'https://open-weather13.p.rapidapi.com/fivedaysforcast?latitude=40.730610&longitude=-73.935242&lang=EN');
+xhr.setRequestHeader('x-rapidapi-key', '398133a7a6msh51384cbccc390e9p1762e6jsnbfab717f1144');
+xhr.setRequestHeader('x-rapidapi-host', 'open-weather13.p.rapidapi.com');
+xhr.setRequestHeader('Content-Type', 'application/json');
 
+xhr.send(data);
 
