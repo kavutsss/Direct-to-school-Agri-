eac59d5f5fa25c43fbf8114f71b9fecf177e0a5f
Erika Kavuto <erika.gwiyo@student.moringaschool.com>
2026-04-19 05:35:56 +0300

    changed name of validation..js yo validation.js

---
+++ validation.js
@@ -0,0 +1,12 @@
+function validateCityInput(input) {
+    const trimmedInput = input.trim();
+    if (trimmedInput === "") {
+        alert ("City name cannot be empty. Please enter a valid city name.");
+        return false;
+    }
+    if (!/^[a-zA-Z\s]+$/.test(trimmedInput)) {
+        alert ("City name can only contain letters and spaces. Please enter a valid city name.");
+        return false;
+    }
+    return true;
+}
